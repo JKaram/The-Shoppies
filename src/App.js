90c969c4b26a187cd834f1d3d52dbcf90b62afbd
root <root@DESKTOP-H8LQ7B1.localdomain>
2020-09-04 18:45:38 -0400

    more setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components/macro';
 import GlobalStyle from './Global';
-import theme from "themes/light";
+import theme from "themes/theme";
+
+import { SearchBar } from "./components"
+import { searchMovies } from "./utils"
 
 function App() {
+  const search = async (text) => {
+    const batman = await searchMovies(text)
+    console.log(batman)
+  }
+
+
+
   return (
     <ThemeProvider theme={theme}>
-
+      <SearchBar />
       <GlobalStyle />
 
       Hello World fdfdsaadfdszsaasadsasdddddddddddd111
