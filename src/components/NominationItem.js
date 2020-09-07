c0ec81d63d2f5bc2d7e0f30ef386692878e24cad
root <root@DESKTOP-H8LQ7B1.localdomain>
2020-09-07 18:57:54 -0400

    add component to render nomination objects

---
+++ src/components/NominationItem.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const NominationItem = styled.div`
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ padding: 24px 24px;
+ margin: 16px 0;
+ background-color: ${p => p.theme["light-blue"]};
+ border-radius: 8px;
+
+ h3 {
+    margin : 0;
+ }
+
+.icon {
+    color: #000;
+    transition: color .3s;
+    cursor: pointer;
+
+    &:hover {
+        color: red;
+    }
+  }
+`
