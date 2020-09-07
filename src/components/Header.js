1294caba862f89326ab0006e6b779ee3f7fb58b4
root <root@DESKTOP-H8LQ7B1.localdomain>
2020-09-07 19:51:19 -0400

    add description to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ export function Header() {
     return (
         <HeaderWrapper>
             <h1>the <span>Shoppies</span></h1>
+            <p>Search and Nominate five movies</p>
         </HeaderWrapper>
     )
 }
@@ -13,6 +14,7 @@ const HeaderWrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
     width: 100%;
     margin: 12px 0;
     padding: 10px 15px;
@@ -20,6 +22,11 @@ const HeaderWrapper = styled.div`
     box-shadow: 0px 0px 30px 5px rgba(183, 183, 183, 0.2);
     background: #fff;
     height: 12vh;
+
+
+    p {
+        margin: 8px;
+    }
 
     h1 {
         margin: 0;
